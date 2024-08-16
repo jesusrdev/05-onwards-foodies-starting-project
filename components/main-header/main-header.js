23bde4847d336e84e99fb3e48f2f7e2510fc95c7
Jesus Rojas <rojasalarcone@gmail.com>
2024-08-16 04:49:23 -0500

    creating a complete project with next

---
+++ components/main-header/main-header.js
@@ -0,0 +1,43 @@
+
+
+import Link from "next/link";
+import Image from "next/image";
+
+
+import MainHeaderBackground from "./main-header-background";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+
+
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            alt="A plate with food on it"
+            width={100}
+            height={100}
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
